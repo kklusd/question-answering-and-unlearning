202d9310b6e6859687c4705d4be842af77320c16
kklu <kanglu8090@gmail.com>
2024-04-18 22:50:59 +0800

    delete

--- model_compression_research/pruning/__init__.py
+++
@@ -1,7 +0,0 @@
-# Apache v2 license
-# Copyright (C) 2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .registry import list_methods, list_schedulers
-from .schedulers import *
-from .methods import *
--- model_compression_research/pruning/schedulers/__init__.py
+++
@@ -1,7 +0,0 @@
-# Apache v2 license
-# Copyright (C) 2021 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .iterative_scheduler import *
-from .one_shot_scheduler import *
-from .scheduler import *
